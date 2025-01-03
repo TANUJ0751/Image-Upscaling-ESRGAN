78259532a86ead2b01d8153da4beb4eb766b2ffa
Tanuj Jain <76273516+TANUJ0751@users.noreply.github.com>
2025-01-03 13:49:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ import torch
 import sys
 import streamlit as st
 import cv2
+from PIL import Image
 sys.path.append('./Real-ESRGAN')
 import RRDBNet_arch as arch
+device = torch.device('cuda')
+st.write("Image Upscaler By tanuj")
+uploaded_image=st.file_uploader("Upload Image file",["JPG","JPEG","PNG"])
+if uploaded_image is not None:
+    image=Image.open(uploaded_image)
+    st.image(image)
+    img = cv2.imread(uploaded_image, cv2.IMREAD_COLOR)
+    img = img * 1.0 / 255
+    img = torch.from_numpy(np.transpose(img[:, :, [2, 1, 0]], (2, 0, 1))).float()
+    img_LR = img.unsqueeze(0)
+    img_LR = img_LR.to(device)
